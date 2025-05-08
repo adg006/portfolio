1258cfea8b9c687f71f27bca81b12773f0d8513d
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-05-08 12:58:55 +0530

    updated navbar component

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,58 +1,139 @@
+import { useRef } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
 export default function Navbar() {
+  const sideMenyRef = useRef();
+
+  const openMenu = () => {
+    sideMenyRef.current.style.transform = "translateX(-16rem)";
+  };
+
+  const closeMenu = () => {
+    sideMenyRef.current.style.transform = "translateX(16rem)";
+  };
+
   return (
-    <nav className="flex items-center justify-between w-full px-5 lg:px-8 xl:px-[8%] py-4 fixed z-50">
-      <a href="#top">
+    <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image
-          src={assets.logo}
-          className="w-28 cursor-pointer mr-14"
-          alt="logo"
+          src={assets.header_bg_color}
+          alt="Background image"
+          className="w-full"
         />
-      </a>
-
-      <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
-        <li>
-          <a href="#top" className="font-Ovo">
-            Home
-          </a>
-        </li>
-
-        <li>
-          <a href="#about" className="font-Ovo">
-            About
-          </a>
-        </li>
-
-        <li>
-          <a href="#services" className="font-Ovo">
-            Services
-          </a>
-        </li>
-
-        <li>
-          <a href="#work" className="font-Ovo">
-            Work
-          </a>
-        </li>
-
-        <li>
-          <a href="#contact" className="font-Ovo">
-            Contact
-          </a>
-        </li>
-      </ul>
-
-      <div>
-        <a
-          href="#contact"
-          className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray rounded-full ml-4"
-        >
-          Contact{" "}
-          <Image src={assets.arrow_icon} className="w-3" alt="Arrow icon" />
-        </a>
       </div>
-    </nav>
+
+      <nav className="flex items-center justify-between w-full px-5 lg:px-8 xl:px-[8%] py-4 fixed z-50">
+        <a href="#top">
+          <Image
+            src={assets.logo}
+            className="w-28 cursor-pointer mr-14"
+            alt="logo"
+          />
+        </a>
+
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+          <li>
+            <a href="#top" className="font-Ovo">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" className="font-Ovo">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#services" className="font-Ovo">
+              Services
+            </a>
+          </li>
+
+          <li>
+            <a href="#work" className="font-Ovo">
+              Work
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" className="font-Ovo">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <div className="flex items-center gap-4">
+          <button>
+            <Image
+              src={assets.moon_icon}
+              alt="Mode changer"
+              className="w-6 cursor-pointer"
+            />
+          </button>
+
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray rounded-full ml-4 font-Ovo"
+          >
+            Contact
+            <Image src={assets.arrow_icon} className="w-3" alt="Arrow icon" />
+          </a>
+
+          <button
+            className="block md:hidden ml-3 cursor-pointer"
+            onClick={openMenu}
+          >
+            <Image src={assets.menu_black} alt="Menu icon" className="w-6" />
+          </button>
+        </div>
+
+        {/* Mobile menu */}
+
+        <ul
+          ref={sideMenyRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li>
+            <a href="#top" className="font-Ovo" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" className="font-Ovo" onClick={closeMenu}>
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#services" className="font-Ovo" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+
+          <li>
+            <a href="#work" className="font-Ovo" onClick={closeMenu}>
+              Work
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" className="font-Ovo" onClick={closeMenu}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
