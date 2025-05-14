a00d46cdb2816e99e32da3c566b5934a272abe61
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-05-14 23:51:36 +0530

    added animation

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
+import { motion } from "motion/react";
 import { assets } from "@/assets/assets";
 
 export default function Contact() {
@@ -29,29 +30,63 @@ export default function Contact() {
   };
 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
       id="contact"
       className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
     >
-      <h4 className="text-center mb-2 text-lg">Connect with me</h4>
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        className="text-center mb-2 text-lg"
+      >
+        Connect with me
+      </motion.h4>
 
-      <h2 className="text-center text-5xl">Get in touch</h2>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl"
+      >
+        Get in touch
+      </motion.h2>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12"
+      >
         I'd love to hear from you! If you have any questions, comments, or
         feedback, please use the form below.
-      </p>
+      </motion.p>
 
-      <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
+      <motion.form
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto"
+      >
         <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
-          <input
+          <motion.input
+            initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
             type="text"
             name="name"
             placeholder="Enter your name"
             className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
             required
           />
-          <input
+          <motion.input
+            initial={{ x: 50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
             type="email"
             name="email"
             placeholder="Enter your email"
@@ -60,24 +95,32 @@ export default function Contact() {
           />
         </div>
 
-        <textarea
+        <motion.textarea
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
           row="6"
           name="message"
           placeholder="Enter your message"
           className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
           required
-        ></textarea>
+        ></motion.textarea>
 
-        <button
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.5 }}
           type="submit"
-          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+          className="py-3 px-8 w-max flex items-center
+          justify-between gap-2 bg-black/80 text-white rounded-full mx-auto
+          hover:bg-black duration-500"
         >
+          {" "}
           Submit now
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
-        </button>
+        </motion.button>
 
         <p className="mt-4">{result}</p>
-      </form>
-    </div>
+      </motion.form>
+    </motion.div>
   );
 }
--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,21 +1,53 @@
 import Image from "next/image";
+import { motion } from "motion/react";
 import { assets, serviceData } from "@/assets/assets";
 
 export default function Services() {
   return (
-    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg">What I Offer</h4>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        className="text-center mb-2 text-lg"
+      >
+        What I Offer
+      </motion.h4>
 
-      <h2 className="text-center text-5xl">My Services</h2>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl"
+      >
+        My Services
+      </motion.h2>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12"
+      >
         I am a front end developer with over 10 years of experience. I have
         worked with companies like Microsoft, Tesla, and Apple.
-      </p>
+      </motion.p>
 
-      <div className="grid grid-cols-auto gap-6 my-10">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.9, delay: 0.6 }}
+        className="grid grid-cols-auto gap-6 my-10"
+      >
         {serviceData.map(({ icon, title, description, link }, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
             key={index}
             className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500"
           >
@@ -33,9 +65,9 @@ export default function Services() {
               Read more
               <Image src={assets.right_arrow} className="w-4" alt="" />
             </a>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
