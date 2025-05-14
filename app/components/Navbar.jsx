286560cbd49d60126ebdd7b5e26c380992fa511f
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-05-14 22:50:59 +0530

    updated navbar component

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -74,14 +74,6 @@ export default function Navbar() {
         </ul>
 
         <div className="flex items-center gap-4">
-          <button>
-            <Image
-              src={assets.moon_icon}
-              alt="Mode changer"
-              className="w-6 cursor-pointer"
-            />
-          </button>
-
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray rounded-full ml-4"
